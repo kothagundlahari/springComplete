021e2bff14d809aea839255b48aa9f6e3aa67122
hari krisha <hari.k.krishna4@gmail.com>
2018-01-11 21:48:35 -0500

    with applicationContext the constructor is called add the load time

---
+++ IocTestJEE/src/test/Client.java
@@ -0,0 +1,20 @@
+package test;
+
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+
+
+public class Client {
+    public static void main(String[] args) {
+
+        ApplicationContext ap = new ClassPathXmlApplicationContext("resource/spring.xml");
+
+
+
+
+    }
+}
