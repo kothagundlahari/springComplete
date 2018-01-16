17c8b0e54932ad86a8be9d17b58127bffd01d18e
hari krisha <hari.k.krishna4@gmail.com>
2018-01-15 22:54:48 -0500

    DI using spring IOC container

---
+++ DI/src/test/Client.java
@@ -0,0 +1,24 @@
+package test;
+
+
+import beans.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+
+
+public class Client {
+    public static void main(String[] args)  {
+
+        ApplicationContext ap = new ClassPathXmlApplicationContext("resource/spring.xml");
+
+        Test t = (Test) ap.getBean("t");
+
+       t.hello("hari");
+
+
+
+
+
+    }
+}
