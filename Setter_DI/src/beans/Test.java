7b62eb9748e323fd46a777175f20b96fc301744a
hari krisha <hari.k.krishna4@gmail.com>
2018-01-16 00:08:07 -0500

    working with int string string in correct order

---
+++ Setter_DI/src/beans/Test.java
@@ -0,0 +1,27 @@
+package beans;
+
+public class Test {
+    
+    private int age;
+    private String eid;
+    private String name;
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setEid(String eid) {
+        this.eid = eid;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void printData() {
+        System.out.println("age:" +age);
+        System.out.println("eid:" +eid);
+        System.out.println("name:" +name);
+    }
+}
+
